2ff82a133c23d35bda0b039c625c29363890d682
Mohammad Arman Soleimani <soleimaniarman98@gmail.com>
2020-12-28 04:04:22 +0330

    Update TFF.vhd

--- TFF.vhd
+++ TFF.vhd
@@ -32,16 +32,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity TFF is
 	port(CLK	:	in	std_logic;
 		  T	:	in std_logic;
+	     	reset	:	in std_logic;
 		  Q	:	inout	std_logic := '0');
 end TFF;
 
 architecture Behavioral of TFF is
 
 begin
-	T_FLIP_FLOP	:	process (CLK)
+	T_FLIP_FLOP	:	process (CLK, reset)
 		begin
-			if(CLK'event and CLK='1') then
-				Q <= not Q;
+			if(reset = '1') then
+				Q <= '0';
+			elsif(CLK'event and CLK='1') then
+				if (T='1') then
+					Q <= not Q;
+				end if;
 			end if;
 		end process;
 
